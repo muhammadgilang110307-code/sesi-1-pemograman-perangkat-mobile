38a31f26ae3f87e15f70e4381be3e456c40b1a97
muhammadgilang110307-code <muhammadgilang110307@gmail.com>
2025-09-22 19:55:35 +0700

    First commit

---
+++ metro.config.js
@@ -0,0 +1,12 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/metro
+ *
+ * @format
+ */
+
+module.exports = {
+  resolver: {
+    sourceExts: ['js', 'jsx', 'json', 'ts', 'tsx'],
+  },
+};
